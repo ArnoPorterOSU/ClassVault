bae62ace391f3c549d93fba57f5c64ea82fc9e6d
Evan-Hock <mocaointegral@gmail.com>
2023-05-12 13:54:15 -0700

    Increased import list

--- frontend/src/Main.elm
+++ frontend/src/Main.elm
@@ -1,3 +1,7 @@
+module Main exposing (..)
+
 import Browser
 import Browser.Navigation as Nav
-import Element exposing (..)
+import Element exposing (Element, Color, Attribute)
+import Element as El
+import Element.Background as Bg
