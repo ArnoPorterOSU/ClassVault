9a68e8974cf631b686390c78ebcc3a38c0a45725
Evan Hock <hocke@oregonstate.edu>
2023-05-13 22:10:19 -0700

    Added Types.elm. This will contain the datatypes used in this project

---
+++ frontend/src/Types.elm
@@ -0,0 +1,31 @@
+module Types exposing (Student, Name, Address)
+
+-- STUDENT TYPE
+
+type alias Student =
+    { name : Name
+    , id : Int
+    , email : String
+    , gpa : Float
+    , address : Address
+    -- TODO: Add attendance records to this somehow
+    }
+
+
+-- NAME TYPE
+
+type alias Name =
+    { first : String
+    , middles : List String
+    , last : String
+    }
+
+
+-- ADDRESS TYPE
+
+type alias Address =
+    { streetAddress : String
+    , zipCode : Int
+    , city : String
+    , state : String
+    }
