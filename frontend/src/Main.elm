45602241158e2810dfd72625cc8b2a3154a6bd18
Evan Hock <hocke@oregonstate.edu>
2023-05-13 22:19:19 -0700

    Moved server back out

--- frontend/src/Main.elm
+++ frontend/src/Main.elm
@@ -6,3 +6,31 @@ import Element.Background as Bg
 import Element.Input as Inp
 import Html exposing (Html)
 import Url exposing (Url)
+import Types exposing (Student)
+
+
+-- MAIN
+
+main : Program Flags Model Msg
+main =
+    Browser.application
+        { init = init
+        , update = update
+        , view = view
+        , subscriptions = subscriptions
+        , onUrlChange = UrlChanged
+        , onUrlRequest = LinkClicked
+        }
+
+
+-- Flags
+
+type alias Flags =
+    { user : String
+    , studentdata : List Student
+    }
+
+
+-- INIT
+
+-- Model
