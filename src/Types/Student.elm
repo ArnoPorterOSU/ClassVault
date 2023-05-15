7b306ed9e56b3a7d1f258e8f761c34ac43e53d9f
Evan Hock <hocke@oregonstate.edu>
2023-05-15 03:30:21 -0700

    Added Address type; updated Student type accordingly

--- src/Types/Student.elm
+++ src/Types/Student.elm
@@ -1,26 +1,17 @@
 module Types.Student exposing
     ( Student
-    , Address
     )
 
 import Types.Email exposing (Email)
 import Types.Name exposing (Name)
+import Types.Address exposing (Address)
 
--- STUDENT TYPE
 
+-- The Student datatype
 type alias Student =
     { name : Name
     , id : Int
     , email : Email
     , gpa : Float
     , address : Address
-    }
-
--- ADDRESS TYPE
-
-type alias Address =
-    { street : String
-    , zipCode : Int
-    , city : String
-    , state : String
     }
