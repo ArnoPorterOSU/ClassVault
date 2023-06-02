0f2f21bd54711acf653e801df67680b68fc662b9
Evan Hock <hocke@oregonstate.edu>
2023-06-02 06:42:00 -0700

    Wrote much of the functionality for the home page

--- src/Page/Home.elm
+++ src/Page/Home.elm
@@ -1,41 +1,55 @@
 module Page.Home exposing
-    ( Model
+    ( Model(..)
+    , init
     , view
     )
 
 
 import Html exposing (Html)
 import Element as El
+import Element.Font as Font
 import Types.Name as Name
 import Types.Student exposing (Student)
 
 
 -- MODEL
-type alias Model =
-    { students : List Student
-    }
+type Model
+    = Loading
+    | Students (List Student)
+    | Error String
 
 
 -- VIEW
 view : Model -> Html msg
 view model =
     El.layoutWith
-        { options = [El.noStaticStyleSheet] }
-        [] <|
-        El.table []
-            { data = model.students
-            , columns =
-                [ { header = El.text "Name"
-                  , width = El.fill
-                  , view = .name >> Name.toString >> El.text
-                  }
-                , { header = El.text "Id Number"
-                  , width = El.fill
-                  , view = .id >> String.fromInt >> El.text
-                  }
-                , { header = El.text "Email"
-                  , width = El.fill
-                  , view = .email >> El.text
-                  }
-                ]
-            }
+        { options = [El.noStaticStyleSheet] } [] <|
+            case model of
+                Students students ->
+                    El.table []
+                        { data = students
+                        , columns =
+                            [ { header = El.text "Name"
+                            , width = El.fill
+                            , view = .name >> Name.toString >> El.text
+                            }
+                            , { header = El.text "Id Number"
+                            , width = El.fill
+                            , view = .id >> String.fromInt >> El.text
+                            }
+                            , { header = El.text "Email"
+                            , width = El.fill
+                            , view = .email >> El.text
+                            }
+                            ]
+                        }
+
+                Loading ->
+                    El.el [Font.size 128] <| El.text "Loading..."
+
+                Error msg ->
+                    El.el [] <| El.text msg
+
+init : Model
+init =
+    Loading
