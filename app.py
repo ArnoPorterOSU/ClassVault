fe7c7ce48e8e13fe7bd6a52ce5982a69ea08d68e
Evan Hock <hocke@oregonstate.edu>
2023-06-02 03:57:17 -0700

    updated app to support more static endpoints for the favicons

--- app.py
+++ app.py
@@ -5,8 +5,20 @@ load_dotenv()
 
 app = Flask(__name__)
 
+STATIC_RESOURCES = (
+    'android-chrome-192x192.png',
+    'android-chrome-512x512.png',
+    'apple-touch-icon.png',
+    'elm.js',
+    'favicon-16x16.png',
+    'favicon32x32.png',
+    'favicon.ico',
+    'site.webmanifest'
+)
 @app.route('/')
 def main():
-    url_for('static', filename='elm.js')
+    for resource in STATIC_RESOURCES:
+        url_for('static', filename=resource)
+
     with open('index.html', 'r') as f:
         return f.read()
