80afa933dc33b6e8bda6290cfba36bf8247dfdec
Evan Hock <hocke@oregonstate.edu>
2023-06-02 05:09:25 -0700

    Cleaned up pages (for now)

--- src/Page/Home.elm
+++ src/Page/Home.elm
@@ -1,51 +1,41 @@
-module Page.Home exposing (..)
+module Page.Home exposing
+    ( Model
+    , view
+    )
 
-import Element as El exposing (Attribute)
-import Element.Background as Bg
-import Element.Input as Inp
+
 import Html exposing (Html)
-import Types.Student exposing (Student)
+import Element as El
 import Types.Name as Name
+import Types.Student exposing (Student)
+
+
+-- MODEL
+type alias Model =
+    { students : List Student
+    }
 
 
 -- VIEW
-
-type Model
-    = Empty
-    | Loading
-    | Table (List Student)
-
-
-noTablePadding : Attribute Msg
-noTablePadding =
-    El.padding 40
-
-loading : Html Msg
-loading =
-    El.layout [ noTablePadding ] <| El.text "Loading..."
-
-noTable : Html Msg
-noTable =
-    El.layout [ noTablePadding ] <|
-        El.column []
-            [ El.text "It looks like there's nothing here"
-            , Inp.button []
-                { onPress = Just <| NewStudent 0
-                , label = El.text "Add Student"
-                }
-            ]
-
-studentTable : List Student -> Html Msg
-studentTable =
-    El.layout [] << El.column [] <<
-        List.map (El.el [ El.mouseOver [ Bg.color <| El.rgb255 128 128 0 ] ] << El.text << Name.toString << .name)
-
-
--- MSG
-
-type Msg
-    = Create Student
-    | Read Student
-    | Update Student
-    | Delete Student
-    | NewStudent Int
+view : Model -> Html msg
+view model =
+    El.layoutWith
+        { options = [El.noStaticStyleSheet] }
+        [] <|
+        El.table []
+            { data = model.students
+            , columns =
+                [ { header = El.text "Name"
+                  , width = El.fill
+                  , view = .name >> Name.toString >> El.text
+                  }
+                , { header = El.text "Id Number"
+                  , width = El.fill
+                  , view = .id >> String.fromInt >> El.text
+                  }
+                , { header = El.text "Email"
+                  , width = El.fill
+                  , view = .email >> El.text
+                  }
+                ]
+            }
--- src/Page/Login.elm
+++
@@ -1,1 +0,0 @@
-module Page.Login exposing (..)
