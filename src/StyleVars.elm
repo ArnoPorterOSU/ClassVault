8b17db4fce8db2df7ed25cc281638b398b1fdb6d
Evan Hock <hocke@oregonstate.edu>
2023-06-10 12:42:37 -0700

    Added StyleVars page for global style constants to be shared between files

---
+++ src/StyleVars.elm
@@ -0,0 +1,19 @@
+module StyleVars exposing
+    ( standardPadding
+    , standardSpacing
+    , white
+    )
+
+import Element exposing (Color, rgb255)
+
+standardPadding : Int
+standardPadding =
+    10
+
+standardSpacing : Int
+standardSpacing =
+    5
+
+white : Color
+white =
+    rgb255 255 255 255
